bd2b5740aae56dd161ae19da670cb831df6449ff
Andre Leveille <andre.joe.leveille@gmail.com>
2020-05-22 12:26:17 -0600

    added model/controller

---
+++ db/migrate/20200522182402_create_accounts.rb
@@ -0,0 +1,12 @@
+class CreateAccounts < ActiveRecord::Migration[6.0]
+  def change
+    create_table :accounts do |t|
+      t.string :name
+      t.float :balance
+      t.string :wish_list
+      t.belongs_to :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
